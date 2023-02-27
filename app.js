786f5bc6087fe409992fca8628459ad602b3f0bd
Abdelrahman Soltan <asoltan@instabug.com>
2023-02-27 22:21:56 +0200

    refactor: ♻️ update tours routes and add temporary user routes

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/data/tours-simple.json`)
 );
 
-// "v1" for API version, which is useful if there's a new version in the future
-app.get('/api/v1/tours', (req, res) => {
+// Route Handlers
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -19,9 +19,8 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
-
-app.get('/api/v1/tours/:id', (req, res) => {
+};
+const getTour = (req, res) => {
   const id = +req.params.id;
   const tour = tours.find((el) => el.id == id);
 
@@ -37,9 +36,8 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
-
-app.post('/api/v1/tours', function (req, res) {
+};
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = {
     id: newId,
@@ -58,9 +56,8 @@ app.post('/api/v1/tours', function (req, res) {
       });
     }
   ); // must be Async to prevent blocking the event loop
-});
-
-app.patch('/api/v1/tours/:id', function (req, res) {
+};
+const updateTour = (req, res) => {
   if (+req.params.id > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -73,9 +70,8 @@ app.patch('/api/v1/tours/:id', function (req, res) {
       tour: '<Updated tour here...>',
     },
   });
-});
-
-app.delete('/api/v1/tours/:id', function (req, res) {
+};
+const deleteTour = (req, res) => {
   if (+req.params.id > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -87,8 +83,54 @@ app.delete('/api/v1/tours/:id', function (req, res) {
     status: 'success',
     data: null,
   });
-});
+};
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+// Routes
+// "v1" for API version, which is useful if there's a new version in the future
+// chaining HTTP methods for the same route
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+// Start Server
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
