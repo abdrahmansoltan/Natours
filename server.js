07bff777c5791defe467a84c5c9b3cb6222fbf59
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-01 17:21:17 +0200

    pkg: 📦 install mongoose and connect to mongoDB database

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 // specify the path of configuration file
 dotenv.config({ path: './config.env' }); // MUST BE BEFORE requiring app file
 
 const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection successful!');
+  })
+  .catch((err) => console.log(err));
 
 // Start Server
 const port = process.env.PORT || 8000;
