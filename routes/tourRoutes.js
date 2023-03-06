f2457c249222cd329fb87990108d81e1d4004543
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-06 08:55:25 +0200

    feat: :sparkles: create tours alias route with added filters middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,11 @@ const tourController = require('../controllers/tourController');
 // Routes
 const router = express.Router();
 
+// Aliasing
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
 // chaining HTTP methods for the same route
 router
   .route('/')
