4a4d2e8adf961d36a9e4f943257c749c98680b85
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-06 08:56:39 +0200

    refactor: :recycle: create APIFeatures class with query methods

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 // Middlewares
 exports.aliasTopTours = (req, res, next) => {
@@ -11,45 +12,13 @@ exports.aliasTopTours = (req, res, next) => {
 // Route Handlers
 exports.getAllTours = async (req, res) => {
   try {
-    // Build the Query
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]); // remove excluded fields from the cloned query object
-
-    // Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`); // find on of these 4 words using regular expression and add "$" to it
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy); // using sort method from Mongoose
-    } else {
-      query = query.sort('-createdAt'); // by default sort tours ascendingly by creation-date
-    }
-
-    // Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields); // include only these fields
-    } else {
-      query = query.select('-__v'); // exclude the "__v" property
-    }
-
-    // Pagination (page=2&limit=10)
-    const page = +req.query.page || 1;
-    const limit = +req.query.limit || 100;
-    const skip = (page - 1) * limit; // "skip" is for the amount of results we skip before looking at data (all results that comes before the requested page)
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      // check if requested page doesn't exist
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error("Page doesn't exist");
-    }
-
     // Execute the Query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // Send response
     res.status(200).json({
