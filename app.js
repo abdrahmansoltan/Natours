75871f9ee346ff395eff6a7036dd0b9c02f02c75
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-08 09:56:44 +0200

    err: 🥅 create error handling middleware and custom AppError class

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -15,5 +17,12 @@ app.use(express.json()); // to have access to "body" of req
 // "v1" for API version, which is useful if there's a new version in the future
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Unhandled Routes
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+// Global Error Handling Middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
