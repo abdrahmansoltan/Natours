bc773b6992b325ac093ba238f2c8f5f085002864
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-11 22:55:45 +0200

    err: 🥅 add error handlers for JWT errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -18,6 +18,12 @@ const handleValidationErrorDB = (err) => {
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
+const handleJWTError = () => {
+  return new AppError('Invalid token, Please log in again!', 401);
+};
+const handleJWTExpiredError = () => {
+  return new AppError('Your token has expired! Please log in again.', 401);
+};
 
 // ----------------Sending Error Response---------------- //
 const sendErrorDev = (err, res) => {
@@ -64,6 +70,8 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
