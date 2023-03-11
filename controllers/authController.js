3fa93ba76c49a31cf0d597b69e319031ad8dc887
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-11 22:58:55 +0200

    feat: :lock: add routes protection & authentication middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util'); // built-in util module
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
@@ -49,4 +50,44 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
   createSendToken(user, 200, res);
+});
+
+// Middleware to protect routes
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+
+  // 2) Token verification
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user belonging to this token no longer exists.', 401)
+    );
+  }
+
+  // 4) Check if user changed password after the token was issued (iat = issuedAt)
+  if (currentUser.isPasswordChangedAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
 });
