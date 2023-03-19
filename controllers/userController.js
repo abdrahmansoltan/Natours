1f9248b40452c0ec5a0e93a6b175f36e1b956062
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-19 09:41:49 +0200

    refactor: :recycle: create factory functions for "get" operations

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,19 +13,6 @@ const filterObj = (obj, ...allowedFields) => {
 };
 
 // Route Handlers
-exports.getAllUsers = catchAsync(async (req, res) => {
-  const users = await User.find();
-
-  // Send response
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -63,12 +50,8 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
 exports.createUser = factory.createOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
