1cf8d0c583addde566d83f3b614567d51cded8bc
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-19 09:45:26 +0200

    fix: :bug: modify "createUser" method to handle undefined route

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,8 +50,14 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined! Please use /signup instead',
+  });
+};
+
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
-exports.createUser = factory.createOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
