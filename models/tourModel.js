bfc5e23f340919b1290f3d497f382646deee7a0a
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-20 13:49:41 +0200

    feat: :sparkles: add a setter function for ratingsAverage field

--- models/tourModel.js
+++ models/tourModel.js
@@ -34,6 +34,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10, // set function that will be run each time a new value is set for ratingsAverage field
     },
     ratingsQuantity: {
       type: Number,
