1926d0e3c4a9b6662cface920365e984f54a7ab7
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-22 18:56:58 +0200

    feat: :sparkles: handle client-side login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -82,7 +82,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookie.jwt) {
+  } else if (req.cookies.jwt) {
     // authenticate users based on tokens send by cookies
     token = req.cookies.jwt;
   }
