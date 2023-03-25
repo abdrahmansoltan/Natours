5e46e71f81a93cb6c190f1760347edf1951d8eea
Abdelrahman Soltan <asoltan@instabug.com>
2023-03-26 00:25:09 +0200

    feat: :sparkles: add signup view controller and routes and signup Pug template

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,14 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
 import { displayMap } from './mapbox';
+import { signup } from './signup';
 import { bookTour } from './stripe';
 import { updateSettings } from './updateSettings';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
+const signupForm = document.querySelector('.form--signup');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
@@ -26,6 +28,18 @@ if (loginForm)
     const password = document.getElementById('password').value;
     login(email, password);
   });
+
+if (signupForm) {
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Dom elements must be defined here after checking that signupForm exists
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    signup(name, email, password, passwordConfirm);
+  });
+}
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
